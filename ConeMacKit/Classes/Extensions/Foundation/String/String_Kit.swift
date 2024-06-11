28c96f792570c9545acad613aaaf9aefac0ca58b
HanQi <ihanqix@gmail.com>
2024-06-11 09:44:42 +0800

    Update String_Kit.swift

--- ConeMacKit/Classes/Extensions/Foundation/String/String_Kit.swift
+++ ConeMacKit/Classes/Extensions/Foundation/String/String_Kit.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CommonCrypto
 
 public extension String {
     
@@ -39,6 +40,16 @@ public extension String {
 }
 
 public extension String {
+    
+    func sha1() -> String {
+        let data = Data(self.utf8)
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
+        }
+        let hexBytes = digest.map { String(format: "%02hhx", $0) }
+        return hexBytes.joined()
+    }
     
     /// 创建随机字符串
     /// - Parameter length: 长度
